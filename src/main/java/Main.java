91e90aaf643517ba90795959f63bfe1376db5f1b
ArtemSh13 <mozg1303@ya.ru>
2024-06-02 15:56:06 +0300

    Add logic of enter of number of persons

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfPersons = -1;
+
+        while (numberOfPersons <= 1) {
+            System.out.println("На скольких человек необходимо разделить счёт?");
+            numberOfPersons = scanner.nextInt();
+            if (numberOfPersons < 1) {
+                System.out.println("Некорректное значение. Нужно ввести целое число больше 1");
+            } else if (numberOfPersons == 1) {
+                System.out.print("Нет смысла ничего считать и делить - вы ели в одиночку!");
+            }
+        }
     }
 }
