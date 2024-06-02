395d70bd906a4227144631789832dceb4a76cd63
ArtemSh13 <mozg1303@ya.ru>
2024-06-03 01:49:08 +0300

    Add logic of enter of goods and prices

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -13,6 +14,23 @@ public class Main {
             } else if (numberOfPersons == 1) {
                 System.out.print("Нет смысла ничего считать и делить - вы ели в одиночку!");
             }
+        }
+
+        String userCommand = "Пока не завершили";
+        HashMap<String, Double> goodsAndPrices = new HashMap<>();
+        double total = 0;
+
+        while (!userCommand.equals("завершить")) {
+            System.out.println("Введите название товара и его стоимость в формате:\n<название товара> - <рубли>.<копейки>");
+            scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            String[] goodAndPrice = userInput.split(" - ");
+            double nextPrice = Double.parseDouble(goodAndPrice[1]);
+            goodsAndPrices.put(goodAndPrice[0], nextPrice);
+            System.out.println("Товар успешно добавлен");
+            total += nextPrice;
+            System.out.println("Введите \"завершить\", чтобы закончить ввод. Чтобы добавить ещё товар, введите любые другие символы");
+            userCommand = scanner.nextLine().toLowerCase();
         }
     }
 }
