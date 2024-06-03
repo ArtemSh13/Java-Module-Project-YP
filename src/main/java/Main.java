694883393dd05799e15da89a3a598cc297163093
ArtemSh13 <mozg1303@ya.ru>
2024-06-03 19:12:27 +0300

    Add logic of output of goods and prices

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +9,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int numberOfPersons = -1;
 
+        // 1. Входные параметры для счётчика
         while (numberOfPersons <= 1) {
             System.out.println("На скольких человек необходимо разделить счёт?");
             numberOfPersons = scanner.nextInt();
@@ -20,6 +24,7 @@ public class Main {
         HashMap<String, Double> goodsAndPrices = new HashMap<>();
         double total = 0;
 
+        // 2. Добавление товаров в калькулятор
         while (!userCommand.equals("завершить")) {
             System.out.println("Введите название товара и его стоимость в формате:\n<название товара> - <рубли>.<копейки>");
             scanner = new Scanner(System.in);
@@ -32,5 +37,26 @@ public class Main {
             System.out.println("Введите \"завершить\", чтобы закончить ввод. Чтобы добавить ещё товар, введите любые другие символы");
             userCommand = scanner.nextLine().toLowerCase();
         }
+
+        // 3. Вывод результатов
+        System.out.println("Добавленные товары:");
+        for (Map.Entry<String, Double> entry : goodsAndPrices.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + "₽");
+        }
+
+        double totalPerPerson = total / numberOfPersons;
+        int totalPerPersonRemainder = (int) totalPerPerson / 10;
+        String rublePostfix = "рублей";
+        if (totalPerPerson  >= 5 && totalPerPerson < 21) {
+            rublePostfix = "рублей";
+        } else {
+            rublePostfix = switch (totalPerPersonRemainder) {
+                case 1 -> "рубль";
+                case 2, 3, 4 -> "рубля";
+                default -> "рублей";
+            };
+        }
+
+        System.out.println("Каждый человек должен заплатить " + String.format("%.2f", totalPerPerson) + " " + rublePostfix);
     }
 }
